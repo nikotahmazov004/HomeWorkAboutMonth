f688c9d20d2aa63c7926d9029fb131add53ed010
Nicat Tahmazov <nijattahmazovv@gmail.com>
2022-08-02 18:41:47 +0400

    Created HomeWork

---
+++ Taaskkkssss/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Taaskkkssss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "aprel";
+            switch (month)
+            {
+                case "yanvar": case "mart": case"may": case "iyul": case"oktyabr": case "dekabr":
+                    Console.WriteLine("31");
+                    break;
+                case "aprel": case "iyun": case "sentyabr": case "noyabr":
+                    Console.WriteLine("30");
+                    break;
+                case "fevral":
+                    Console.WriteLine("28");
+                    break; 
+            }
+        }
+    }
+}
